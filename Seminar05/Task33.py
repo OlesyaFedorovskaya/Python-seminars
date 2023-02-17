fdd52125c93d788284468f383ac54a2c5b29074f
OlesyaFedorovskaya <nyamka13@mail.ru>
2023-02-17 06:40:21 +0300

    added information to the Task33.py

--- Seminar05/Task33.py
+++ Seminar05/Task33.py
@@ -9,3 +9,15 @@
 
 import os
 os.system('cls')
+
+from random import randint
+
+my_list = [randint(1,5) for _ in range(randint(3,7))]
+print(my_list)
+
+max_elem = max(my_list)
+min_elem = min(my_list)
+for i in range(len(my_list)):
+    if my_list[i] == max_elem:
+        my_list[i] = min_elem
+print(my_list)
