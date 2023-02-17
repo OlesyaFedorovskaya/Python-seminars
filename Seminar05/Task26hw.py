2d69a204e822295d5d36191e6ac65a9f1646046c
OlesyaFedorovskaya <nyamka13@mail.ru>
2023-02-17 04:34:27 +0300

    added information to the Task26hw.py

--- Seminar05/Task26hw.py
+++ Seminar05/Task26hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+import os
+os.system('cls')
+
+def Exponentiation(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    if b != 1:
+        return a * Exponentiation(a, b - 1)
+
+a = int(input('Введите число: '))
+b = int(input('Введите степень: '))
+print(f'Число {a} в степени {b} = {Exponentiation(a, b)}')
